869cd9344e4d1f38773e81af8c19a489109b5e03
eldos-dev <eldossharsheev00@gmail.com>
2021-02-20 17:58:15 +0600

    First commit

---
+++ parser.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import lxml
+
+# Функция принимает url, возвращает html text
+def get_html(url):
+    user_agent = {"User-Agent":"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
+    response = requests.get(url, headers=user_agent)
+    return response.text
+
+# Принимает data и записывает в json файл
+def write_to_json(data):
+    with open('mashina_kg.json', 'w') as json_file:
+        json.dump(data, json_file, indent=3)
+
+# Принимает html text, парсит нужные информации и вызывает функцию write_to_json для записи в  файл json
+def get_data_pages(html):
+    soup = BeautifulSoup(html, 'lxml')
+    wrap_div = soup.find('div', class_='search-results-table').find('div', class_='table-view-list')
+    products = wrap_div.find_all('div', class_='list-item')
+
+    list_data = []
+    for product in products:
+        try:
+            title = product.find('a').find('h2').text.strip()
+        except:
+            title = ''
+        try:
+            image = product.find('img').get('data-src').strip()
+        except:
+            image = ''
+        try:
+            price = product.find('p', class_='price').find('strong').text
+            year = product.find('p', class_='year-miles').find('span').text.strip()
+            fuel = product.find('p', class_='body-type').text.strip()
+            wheel = product.find('p', class_='volume').text.strip().split()
+            # city = product.find('p', class_='city').text.strip()
+            description = {
+                            'price': price,
+                            'year': year,
+                            'fuel': fuel, 
+                            'wheel': wheel[1]}
+        except:
+            description = ''
+        
+        data = {
+                'title': title,
+                'image': image,
+                'description': description}
+        list_data.append(data)
+    write_to_json(list_data)
+
+# Очищаем csv файл перед записанием информаци.
+# def clean_json_file():
+#     with open('mashina_kg.json', 'w') as my_empty_json_file:
+#             my_empty_json_file.write('')
+
+# pass_car
+def main(url):
+    main_url = 'https://www.mashina.kg/'
+
+    get_data_pages(get_html(main_url + url))
--- tele_bot.py
+++ tele_bot.py
@@ -1,6 +1,6 @@
 from my_bot_token import TOKEN
 from telebot import types
-from parsing import main
+from parser import main
 import telebot
 import json
 
@@ -8,6 +8,7 @@ token = TOKEN
 
 bot = telebot.TeleBot(token)
 
+# open and read json file
 def get_dict():
     json_file = open('mashina_kg.json')
     data = json.load(json_file)
@@ -15,41 +16,74 @@ def get_dict():
     list_dict = [dict(elem) for elem in data]
     return list_dict
 
-# Create buttons
-inline_keyboard = types.InlineKeyboardMarkup()
+# Create buttons category
+reply_keyboards = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=2)
+k1 = types.KeyboardButton('Легковые авто')
+k2 = types.KeyboardButton('Коммерческие авто')
+k3 = types.KeyboardButton('Спецтехника')
+k4 = types.KeyboardButton('Мотоциклы')
+reply_keyboards.add(k1, k2, k3, k4)
 
+# Start message
 @bot.message_handler(commands=['start', 'hello'])
-def start(message):
-    main()
+def start_message(message):
     chat_id = message.chat.id
+    msg = bot.send_message(chat_id, f'Выберите категорию:', reply_markup=reply_keyboards)
+    bot.register_next_step_handler(msg, get_func)
 
+# Choice category
+def get_func(message):
+    if message.text == 'Легковые авто':
+        url = 'search/all/'
+        main(url)
+    if message.text == 'Коммерческие авто':
+        main('commercialsearch/all/')
+    if message.text == 'Спецтехника':
+        main('specsearch/all/')
+    if message.text == 'Мотоциклы':
+        main('motosearch/all/')
+    get_start(message)
+
+# Create buttons
+def get_inline_keyboard():
+    inline_keyboard = types.InlineKeyboardMarkup()
     list_dict = get_dict()
     count = 1
     for dict_ in list_dict:
         batton = types.InlineKeyboardButton(f"{count} - {dict_.get('title')}", callback_data=f'{count}')
         count += 1
         inline_keyboard.add(batton)
-    bot.send_message(chat_id, f'Приветсвую, {message.chat.first_name}. Выберите машину', reply_markup=inline_keyboard)
+    return inline_keyboard
+
+
+def get_start(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, f'Приветсвую, {message.chat.first_name}. Выберите машину', reply_markup=get_inline_keyboard())
 
 # Create buttons 'back' & 'exit'
 keyboards = types.InlineKeyboardMarkup()
-btn1 = types.InlineKeyboardButton('Назад', callback_data='back')
-btn2 = types.InlineKeyboardButton('Выйти', callback_data='exit')
-keyboards.add(btn1, btn2)
+btn1 = types.InlineKeyboardButton('Назад', callback_data='back to list')
+btn2 = types.InlineKeyboardButton('Вернуться в категорию', callback_data='back to category')
+btn3 = types.InlineKeyboardButton('Выйти', callback_data='exit')
+keyboards.add(btn1, btn2, btn3)
 
 @bot.callback_query_handler(func=lambda c: True)
 def show_discription(c):
     chat_id = c.message.chat.id
-    if c.data == 'back':
-        bot.send_message(chat_id, 'Вы вернулись в список', reply_markup=inline_keyboard)
+    if c.data == 'back to list':
+        bot.edit_message_text('Вы вернулись в список', chat_id, c.message.message_id, reply_markup=get_inline_keyboard())
+    # elif c.data == 'back to category':
+    #     bot.edit_message_text('Вы вернулись в категорию', chat_id, c.message.message_id, reply_markup=start_message)
     elif c.data == 'exit':
-        bot.send_message(chat_id, 'Досвидания', reply_markup=None)
+        bot.edit_message_text(f'Досвидания, наш дорогой пользователь!', chat_id, c.message.message_id, reply_markup=None)
+        bot.send_sticker(chat_id, 'CAACAgIAAxkBAAK5TmAJEsAJxfQhYwOJFvrcYJqeVJJPAAJhAANOXNIpSmNxpUypVW4eBA')
     else:
         list_dict = get_dict()
         dict_ = list(list_dict[int(c.data) - 1].values())
         dict_2 = list(dict_[2].values())
-        bot.send_message(chat_id,
-                        f'Модель:  {dict_[0]}\n фото:  {dict_[1]} \n Цена:  {dict_2[0]} \nГод:  {dict_2[1]} \nДвигатель:  {dict_2[2]} \nРуль:  {dict_2[3]}',
+        bot.edit_message_text(f'Модель:  {dict_[0]}\n фото:  {dict_[1]} \n Цена:  {dict_2[0]} \nГод:  {dict_2[1]} \nДвигатель:  {dict_2[2]} \nРуль:  {dict_2[3]}',
+                        chat_id,
+                        c.message.message_id,
                         reply_markup=keyboards)
 
 bot.polling()
